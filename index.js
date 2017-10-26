34206fcdfc4e30dd43a22fce02318759c431fa44
BtheGit <brendanwebdev@gmail.com>
2017-10-25 21:01:24 -0400

    Added in scroll animation to work cards

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const buttonContact = document.getElementById('button-contact')
 const buttonReset = document.getElementById('button-reset')
 
 const splashBG = document.getElementById('splash-bg')
-document.addEventListener('scroll', e => {console.log(e)})
 const workContainer = document.getElementById('work-container')
 const contactContainer = document.getElementById('contact-container')
 const aboutContainer = document.getElementById('about-container')
+
+const workScrollable = document.getElementById('work-scrollable')
+const workCards = document.querySelectorAll('.work-card')
+workScrollable.addEventListener('scroll', debounce(scrollCards))
 
 const bokehField = bokehfy({
   parent:splashBG,
@@ -60,6 +63,7 @@ function selectWork(e) {
   toggleButtons()
   splashBG.classList.add('splash-right')
   workContainer.classList.add('active')
+  setTimeout(scrollCards, 500)
 }
 
 function selectContact(e) {
@@ -86,6 +90,8 @@ function resetSplash(e) {
       className.classList.remove('active')
     }
   })
+  workScrollable.scrollTop = 0
+  workCards.forEach(card => card.classList.remove('visible'))
 }
 
 function toggleButtons(e) {
@@ -94,13 +100,38 @@ function toggleButtons(e) {
   })
 }
 
-
+function scrollCards() {
+  const halfCardHeight = 350 / 2;
+  const target = window.innerHeight - workScrollable.offsetTop - halfCardHeight
+  workCards.forEach(el => {
+    if (el.getBoundingClientRect().top < target) el.classList.add('visible')
+  })
+}
 
 
 document.addEventListener('DOMContentLoaded', ()=> {
   console.log('%cWelcome to my portfolio!', "color: #00B0AE; font-size: 20px")
-  console.log("I'm currently accepting offers for Frontend, Backend, or Full Stack positions.")
-  console.log("I'm currently based in Washington DC but am open to relocate across America or Europe.")
+  console.log("I'm currently looking for an awesome Frontend, Backend, or Full Stack position.")
+  console.log("I'm based in Washington DC but am open to relocate across America or Europe.")
   console.log("%cbrendanbeltz@gmail.com", "font-size: 20px; color: purple;")
   console.log("%c(571) 699-9612", "font-size: 19px; color: #AC02AC;")
 })
+
+
+// ##################### UTILITIES
+
+
+function debounce(func, wait = 5, immediate) {
+	let timeout;
+	return function() {
+		const context = this, args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
